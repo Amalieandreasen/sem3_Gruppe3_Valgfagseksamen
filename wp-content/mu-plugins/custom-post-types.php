630c4fb14cdbfe22deeb1487b15266e74980ac5a
Amalie <amalie.andreasen99@hotmail.com>
2024-09-17 11:20:19 +0200

    Race custom post type
    Custom post type race tilføjet + installation af acf

--- wp-content/mu-plugins/custom-post-types.php
+++ wp-content/mu-plugins/custom-post-types.php
@@ -5,19 +5,45 @@ function customPostTypes()
     // Her registrere vi vores custom post type for alle dyr
     register_post_type('dyr', array(
         'show_in_rest' => true,
-        'supports' => array('title', 'editor'),
+        'supports' => array('title', 'editor', 'thumbnail'),
         'rewrite' => array('slug' => 'dyr'),
         'has_archive' => true,
         'public' => true,
         'labels' => array(
-            'name' => 'dyr',
+            'name' => 'Dyr',
             'add_new_item' => 'Tilføj nyt dyr',
             'edit_item' => 'Rediger dyr',
             'all_items' => 'Alle dyr',
-            'singular_name' => 'dyr'
+            'singular_name' => 'Dyr'
         ),
         'menu_icon' => 'dashicons-pets'
     ));
+
+    // Her registrere vi vores custom post type for alle dyr
+    register_post_type('race', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'rewrite' => array('slug' => 'race'),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Racer',
+            'add_new_item' => 'Tilføj ny race',
+            'edit_item' => 'Rediger race',
+            'all_items' => 'Alle racer',
+            'singular_name' => 'Race'
+        ),
+        'menu_icon' => 'dashicons-list-view'
+    ));
+}
+
+function dyrFeatures()
+{
+    add_theme_support('post-thumbnails');
+    add_image_size('dyrImg', 500, 400, true);
+    add_image_size('dyrCard', 200, 200, true);
 }
 
 add_action('init', 'customPostTypes');
+
+add_action('after_setup_theme', 'dyrFeatures');
