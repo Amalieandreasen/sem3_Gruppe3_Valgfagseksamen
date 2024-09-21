73e880bdd0edf15bf23a6f316d972e5d75b609b3
Amalie <amalie.andreasen99@hotmail.com>
2024-09-21 12:32:52 +0200

    fjernet ubrugt js fil

--- wp-content/themes/Sejr og Davidsens tema/functions.php
+++ wp-content/themes/Sejr og Davidsens tema/functions.php
@@ -6,7 +6,6 @@ function customThemeFiles()
     wp_enqueue_script('index.js', get_theme_file_uri('/JS/index.js'), array(), null, true);
     wp_enqueue_script('adoptAnimal.js', get_theme_file_uri('/JS/adoptAnimal.js'), array(), null, true);
     wp_enqueue_script('bookOphold.js', get_theme_file_uri('/JS/bookOphold.js'), array(), null, true);
-    wp_enqueue_script('filter.js', get_theme_file_uri('/JS/filter.js'), array(), null, true);
 }
 
 add_action('wp_enqueue_scripts', 'customThemeFiles');
