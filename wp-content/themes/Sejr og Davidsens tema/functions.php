904c1c637838678db17e1e06b974eb832c1048fa
Amalie <amalie.andreasen99@hotmail.com>
2024-09-21 10:28:03 +0200

    fixet main error
    Vi har flyttet vores kommentar ind i php så vores kode nu formentligt virker og kan komme online.

--- wp-content/themes/Sejr og Davidsens tema/functions.php
+++ wp-content/themes/Sejr og Davidsens tema/functions.php
@@ -1,11 +1,12 @@
-<!-- Her henter vi vores stylesheets og Javascript filer og aktiviterer dem med en add_action -->
 <?php
+// Her henter vi vores stylesheets og Javascript filer og aktiverer dem med en add_action
 function customThemeFiles()
 {
     wp_enqueue_style('globalStyles', get_theme_file_uri('/css/globalStyle.css'));
     wp_enqueue_script('index.js', get_theme_file_uri('/JS/index.js'), array(), null, true);
     wp_enqueue_script('adoptAnimal.js', get_theme_file_uri('/JS/adoptAnimal.js'), array(), null, true);
     wp_enqueue_script('bookOphold.js', get_theme_file_uri('/JS/bookOphold.js'), array(), null, true);
+    wp_enqueue_script('filter.js', get_theme_file_uri('/JS/filter.js'), array(), null, true);
 }
 
 add_action('wp_enqueue_scripts', 'customThemeFiles');
